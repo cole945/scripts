01ff3d7e5da91477d6c78b86e67cdb0f91d0b026
Cole Wang <cole945@gmail.com>
2020-05-26 13:52:07 +0800

    ffmpeg

--- ffmpeg-enc.sh
+++ ffmpeg-enc.sh
@@ -48,8 +48,15 @@ if [[ ${Q} -gt 51 ]]; then
     # Two-pass bitrate
     ## ffmpeg -y -i MOV_0465.mp4 -preset fast -c:v libx265 -b:v 1500k -x265-params pass=1 -an -f mp4 /dev/null
     ## ffmpeg -i MOV_0465.mp4 -c:v libx265 -b:v 1500k -x265-params pass=2 -c:a aac -b:a 128k output.mp4
-    ffmpeg -y -i "${FILE}" -preset fast -c:v ${CODEC} -b:v ${Q}k -x265-params pass=1 -an -f mp4 /dev/null
-    ffmpeg -y -i "${FILE}" -c:v ${CODEC} ${ARG_SCALE} -b:v ${Q}k -x265-params pass=2 -c:a ${ARG_ACODEC} ${FILE_BASE}-${CODEC}-${Q}.${FILE_EXT} 2>&1
+    if [ $CODEC == "libx265" ]; then
+      ffmpeg -y -i "${FILE}" -c:v ${CODEC} ${ARG_SCALE} -b:v ${Q}k -x265-params no-slow-firstpass=1:pass=1 -an -f mp4 /dev/null
+      ffmpeg -y -i "${FILE}" -c:v ${CODEC} ${ARG_SCALE} -b:v ${Q}k -x265-params pass=2 -c:a ${ARG_ACODEC} ${FILE_BASE}-${CODEC}-${Q}.${FILE_EXT} 2>&1
+    elif [ $CODEC == "libx264" ]; then
+      ffmpeg -y -i "${FILE}" -c:v ${CODEC} ${ARG_SCALE} -b:v ${Q}k -pass 1 -fastfirstpass 1 -an -f mp4 /dev/null
+      ffmpeg -y -i "${FILE}" -c:v ${CODEC} ${ARG_SCALE} -b:v ${Q}k -pass 2 -c:a ${ARG_ACODEC} ${FILE_BASE}-${CODEC}-${Q}.${FILE_EXT} 2>&1
+    else
+      ffmpeg -y -i "${FILE}" -c:v ${CODEC} ${ARG_SCALE} -b:v ${Q}k -c:a ${ARG_ACODEC} ${FILE_BASE}-${CODEC}-${Q}.${FILE_EXT} 2>&1
+    fi
 else
     # CRF
     # ffmpeg -i "${FILE}" -c:v libx265 -c:a aac -b:a 128k output-crf24.mp4
